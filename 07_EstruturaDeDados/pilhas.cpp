62a4d933fa6971d317df51eb230afdbc291d02e5
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-08-31 21:11:55 -0300

    update pilhas.cpp data_structure

--- 07_EstruturaDeDados/pilhas.cpp
+++ 07_EstruturaDeDados/pilhas.cpp
@@ -4,31 +4,145 @@
 //
 // Created by Henrique Souza on 28/08/2022.
 //
-struct PILHA
+struct Item
 {
     int numero;
-    PILHA *proximo;
+    struct Item *proximo;
 };
+
+void inicializando_pilha(Item **topo)
+{
+    *topo = NULL;
+}
+
+bool pilha_vazia(Item **topo)
+{
+    if (*topo == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void empilhar(Item **topo, int elemento)
+{
+    Item *novo;
+
+    novo = (Item *)malloc(sizeof(Item));
+
+    novo->numero = elemento;
+
+    // -> proximo recebe o elemento que estava no topo.
+    novo->proximo = *topo;
+
+    *topo = novo;
+}
+
+int desempilhar(Item **topo)
+{
+    int result;
+    Item *auxiliar;
+
+    if (pilha_vazia(topo))
+    {
+        printf("\n stack underflow! \n");
+
+        exit(1);
+    }
+    else
+    {
+        // -> Elemento retirado do topo
+        result = (*topo)->numero;
+
+        auxiliar = *topo;
+
+        *topo = (*topo)->proximo;
+
+        free(auxiliar);
+
+        return result;
+    }
+}
+
+void imprimir_pilha(Item *topo)
+{
+    int i = 0;
+    Item *item;
+
+    printf("\n\n Listando...\n\n");
+    printf("---------------------------------\n");
+
+    if (pilha_vazia(&topo))
+    {
+        printf("A Pilha esta vazia!\n");
+    }
+    else
+    {
+        item = topo;
+
+        while (item != NULL)
+        {
+            i++;
+
+            printf("[%i] -> %i\n", i, item->numero);
+
+            item = item->proximo;
+        }
+    }
+    printf("---------------------------------\n");
+}
+
+void menu()
+{
+    printf("\n\n\t\tMENU DE OPCOES\n\n"
+           "1.\tEmpilhar elemento\n"
+           "2.\tDesempilhar\n"
+           "0.\t Sair\n"
+           "\n\nEscolha uma das opcoes: ");
+}
 
 int main()
 {
-    // inicializando pilha
-    PILHA *topo = NULL;
-    // ponteiro auxiliar
-    PILHA *auxiliar;
+    Item *topo = NULL;
+    int opcao, numero;
 
-    int opcoes;
+    menu();
+    scanf("%i", &opcao);
 
-    do
+    while (opcao != 0)
     {
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        scanf
-    } while (/* condition */);
+        switch (opcao)
+        {
+        case 1:
+            printf("Digite um numero: ");
+            scanf("\n%i", &numero);
 
+            // Empilhando o dado
+            empilhar(&topo, numero);
+
+            // Imprimindo pilha armazenada
+            imprimir_pilha(topo);
+
+            break;
+        case 2:
+            numero = desempilhar(&topo);
+
+            printf("\n Numero desempilhado: %d \n", numero);
+
+            imprimir_pilha(topo);
+
+            break;
+        default:
+            printf("Escolha invalida.\n\n");
+
+            break;
+        }
+        menu();
+        scanf("%i", &opcao);
+    }
+    system("cls");
+    system("pause");
 }
