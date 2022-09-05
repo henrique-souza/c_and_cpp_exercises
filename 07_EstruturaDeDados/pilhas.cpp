9e9ab17f871fe922f1040839616e7e0a692b4fcf
Henrique Souza <hs.santos@totvs.com.br>
2022-09-05 17:46:48 -0300

    update datastructure

--- 07_EstruturaDeDados/pilhas.cpp
+++ 07_EstruturaDeDados/pilhas.cpp
@@ -4,18 +4,18 @@
 //
 // Created by Henrique Souza on 28/08/2022.
 //
-struct Item
+struct PILHA
 {
     int numero;
-    struct Item *proximo;
+    struct PILHA *proximo_elemento;
 };
 
-void inicializando_pilha(Item **topo)
+void inicializando_pilha(PILHA **topo)
 {
     *topo = NULL;
 }
 
-bool pilha_vazia(Item **topo)
+bool pilha_vazia(PILHA **topo)
 {
     if (*topo == NULL)
     {
@@ -27,24 +27,35 @@ bool pilha_vazia(Item **topo)
     }
 }
 
-void empilhar(Item **topo, int elemento)
+bool pilha_cheia(PILHA **topo)
 {
-    Item *novo;
-
-    novo = (Item *)malloc(sizeof(Item));
-
-    novo->numero = elemento;
-
-    // -> proximo recebe o elemento que estava no topo.
-    novo->proximo = *topo;
-
-    *topo = novo;
+    if (*topo != NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-int desempilhar(Item **topo)
+void empilhar(PILHA **topo, int elemento)
+{
+    PILHA *novo_elemento;
+
+    novo_elemento = (PILHA *)malloc(sizeof(PILHA));
+
+    novo_elemento->numero = elemento;
+
+    novo_elemento->proximo_elemento = *topo;
+
+    *topo = novo_elemento;
+}
+
+int desempilhar(PILHA **topo)
 {
     int result;
-    Item *auxiliar;
+    PILHA *auxiliar;
 
     if (pilha_vazia(topo))
     {
@@ -59,7 +70,7 @@ int desempilhar(Item **topo)
 
         auxiliar = *topo;
 
-        *topo = (*topo)->proximo;
+        *topo = (*topo)->proximo_elemento;
 
         free(auxiliar);
 
@@ -67,12 +78,12 @@ int desempilhar(Item **topo)
     }
 }
 
-void imprimir_pilha(Item *topo)
+void imprimir_pilha(PILHA *topo)
 {
     int i = 0;
-    Item *item;
+    PILHA *item;
 
-    printf("\n\n Listando...\n\n");
+    printf("\n\tPILHA\n\n");
     printf("---------------------------------\n");
 
     if (pilha_vazia(&topo))
@@ -89,7 +100,7 @@ void imprimir_pilha(Item *topo)
 
             printf("[%i] -> %i\n", i, item->numero);
 
-            item = item->proximo;
+            item = item->proximo_elemento;
         }
     }
     printf("---------------------------------\n");
@@ -97,16 +108,17 @@ void imprimir_pilha(Item *topo)
 
 void menu()
 {
-    printf("\n\n\t\tMENU DE OPCOES\n\n"
-           "1.\tEmpilhar elemento\n"
+    printf("\n\n\tMENU DE OPCOES\n\n"
+           "1.\tEmpilhar\n"
            "2.\tDesempilhar\n"
-           "0.\t Sair\n"
+           "0.\tSair\n"
            "\n\nEscolha uma das opcoes: ");
 }
 
 int main()
 {
-    Item *topo = NULL;
+    PILHA *topo = NULL;
+
     int opcao, numero;
 
     menu();
@@ -123,12 +135,15 @@ int main()
             // Empilhando o dado
             empilhar(&topo, numero);
 
+            system("cls");
             // Imprimindo pilha armazenada
             imprimir_pilha(topo);
 
             break;
         case 2:
             numero = desempilhar(&topo);
+
+            system("cls");
 
             printf("\n Numero desempilhado: %d \n", numero);
 
