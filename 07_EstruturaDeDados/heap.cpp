f802b83b138b2ed5237d52888b77f15a2c1f2f55
Henrique Souza <h.s.s_henrique@hotmail.com>
2022-09-24 15:33:20 -0300

    Update heap.cpp

--- 07_EstruturaDeDados/heap.cpp
+++ 07_EstruturaDeDados/heap.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <bits/stdc++.h>
 #define POSITIONS 10
 
 //
@@ -18,6 +19,44 @@ int left_child(int *heap_value, int position)
 int right_child(int *heap_value, int position)
 {
     return heap_value[position * 2];
+}
+
+void heapify(int array_in[], int array_index, int subtree_root_index)
+{
+    // Initialize largest_value as root
+    int largest_value = subtree_root_index;
+    int left_child = 2 * subtree_root_index + 1;
+    int right_child = 2 * subtree_root_index + 2;
+
+    // If left child is larger than root
+    if (left_child < array_index && array_in[left_child] > array_in[largest_value])
+    {
+        largest_value = left_child;
+    }
+
+    // If right child is larger than largest_value so far
+    if (right_child < array_index && array_in[right_child] > array_in[largest_value])
+    {
+        largest_value = right_child;
+    }
+
+    // If largest_value is not root
+    if (largest_value != subtree_root_index)
+    {
+        std::swap(array_in[subtree_root_index], array_in[largest_value]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(array_in, array_index, largest_value);
+    }
+}
+
+void print_heap(int array_in[], int array_size)
+{
+    printf("\n\t\tHEAP TREE BELOW\n\n");
+    for (int index = 0; index < array_size; index++)
+    {
+        printf("%d\t", array_in[index]);
+    }
 }
 
 int main()
@@ -44,20 +83,24 @@ int main()
             }
             else
             {
-                printf("\n\tValor do filho da esquerda: %d", left_child(heap, chosen_position));
+                printf("\n\tValor do filho da esquerda: %d do No: %d", left_child(heap, chosen_position), heap[chosen_position - 1]);
             }
             if (chosen_position * 2 >= 10)
             {
                 printf("\n\tO No '%d' nao tem filho da direita\n", heap[chosen_position - 1]);
             }
             else
+
             {
-                printf("\n\tValor do filho da direita: %d\n", right_child(heap, chosen_position));
+                printf("\n\tValor do filho da direita: %d do No: %d\n", right_child(heap, chosen_position), heap[chosen_position - 1]);
             }
         }
         if (chosen_position < 0 || chosen_position > POSITIONS)
         {
             printf("\nPosicao invalida");
         }
+
+        print_heap(heap, POSITIONS);
+
     } while (chosen_position != 0);
 }
