1e9094109f486059af67e68cebc5b1159597cc16
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-10-08 22:55:05 -0300

    update heap.cpp exercise

--- 07_EstruturaDeDados/heap.cpp
+++ 07_EstruturaDeDados/heap.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 
 //
-// Created by Henrique Souza on 05/09/2022.
-// Graphic representation of this algorithm in ./README.md
+// Created by Henrique Souza on 08/10/2022.
 //
 
 struct Node {
@@ -11,23 +10,32 @@ struct Node {
 };
 
 struct Node *new_node(int value) {
-    struct Node *temp = (struct Node *) malloc(sizeof(struct Node));
+    auto *temp = (struct Node *) malloc(sizeof(struct Node));
     temp->value = value;
-    temp->left_child = temp->right_child = NULL;
+    temp->left_child = temp->right_child = nullptr;
     return temp;
 }
 
-// Imprime os dados armazenados na árvore
-void print_heap(int array_in[], int array_size) {
-    printf("\n\t\tHEAP TREE BELOW\n\n");
-    for (int index = 0; index < array_size; index++) {
-        printf("%d\t", array_in[index]);
+// Imprime os dados armazenados na árvore percorrendo-a em ordem
+void print_heap_in_order(struct Node *root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    if (root->left_child != nullptr) {
+        print_heap_in_order(root->left_child);
+    }
+
+    printf("%d ", root->value);
+
+    if (root->right_child != nullptr) {
+        print_heap_in_order(root->right_child);
     }
 }
 
 // Função que insere valor na árvore
 struct Node *insert_value(struct Node *father, int value) {
-    if (father == NULL) return new_node(value);
+    if (father == nullptr) return new_node(value);
 
     if (value < father->value) {
         father->left_child = insert_value(father->left_child, value);
@@ -37,49 +45,20 @@ struct Node *insert_value(struct Node *father, int value) {
     return father;
 }
 
-// Função que deleta a raiz da árvore
-//void deleteRoot(int array_in[], int array_index, int chosen_element) {
-//    // Get the last element
-//    chosen_element = array_in[array_index - 1];
-//
-//    // Replace root with last element
-//    array_in[0] = chosen_element;
-//
-//    // Decrease size of heap by 1
-//    array_index = array_index - 1;
-//
-//    // heapify the root node
-//    heapify(array_in, array_index, 0);
-//}
-
 // Menu da aplicação
 void menu() {
     printf("\n\n\tMENUS DE OPCOES\n"
-           "1.\tConsultar valores do Heap\n"
-           "2.\tInserir valor\n"
-           "3.\tDeletar valor\n"
-           "4.\tImprimir heap\n"
+           "1.\tImprimir heap\n"
            "0.\tSair\n"
            "\n\nEscolha uma das opcoes: ");
 }
 
-// Percorrendo a árvore em ordem
-void inorder_walk(struct Node *root) {
-    if (root == NULL) {
-        return;
-    }
-    if (root->left_child != NULL) {
-        inorder_walk(root->left_child);
-    }
-    printf("%d ", root->value);
-    if (root->right_child != NULL) {
-        inorder_walk(root->right_child);
-    }
-}
-
 int main() {
-    int option, chosen_position;
-    // heap = {17, 12, 8, 5, 3, 6, 2, 4, 2, 1};
+    int option;
+    struct Node *root = nullptr;
+
+    // Fazendo atribuição do tamanho do Heap (10)
+    root = insert_value(root, 10);
 
     do {
         menu();
@@ -89,15 +68,25 @@ int main() {
             case 0:
                 exit(0);
             case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
+                // Inserindo os valores aleatóriamente
+                insert_value(root, 17);
+                insert_value(root, 12);
+                insert_value(root, 8);
+                insert_value(root, 5);
+                insert_value(root, 3);
+                insert_value(root, 6);
+                insert_value(root, 2);
+                insert_value(root, 4);
+                insert_value(root, 2);
+                insert_value(root, 1);
+
+                // Imprimindo em ordem
+                print_heap_in_order(root);
+
+                printf("\n\n");
                 break;
             default:
                 break;
         }
-    } while (chosen_position != 0);
+    } while (true);
 }
