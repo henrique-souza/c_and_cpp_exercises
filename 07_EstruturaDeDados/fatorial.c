238a3a962afa539f08d536df60650e0e334f4f63
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-09-10 15:12:36 -0300

    recursive call exercise

--- 07_EstruturaDeDados/fatorial.c
+++ 07_EstruturaDeDados/fatorial.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <time.h>
+
+// declarando a função que calcula o fatorial do numero
+int calcularFatorial(int produto, int numero);
 
 int main()
 {
     int produto, numero;
+    produto = 1;
     clock_t start_time = clock();
 
-    produto = 1;
-
     printf("Digite um número para saber seu fatorial: ");
-    scanf("%d", &numero);
 
-    for (size_t x = numero; x > 0; x--)
+    if (scanf("%d", &numero) != 1)
     {
-        produto *= x;
+        printf("Erro ao ler o número.\n");
+        // Encerra o programa com um código de erro.
+        return 1;
     }
 
-    printf("Fatorial do número %d eh: %d", numero, produto);
+    printf("Fatorial do número %d eh: %d", numero, calcularFatorial(numero, produto));
 
     double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
 
     printf("\nDone in %f seconds", elapsed_time);
 
     return 0;
+}
+
+int calcularFatorial(int numero, __attribute__((unused)) int produto)
+{
+    int fatorial, x, y;
+    y = 0;
+
+    if (numero == 0)
+    {
+        fatorial = 1;
+    }
+    else
+    {
+        // definicao de n!
+        x = numero - 1;
+        y = calcularFatorial(x, y);
+        fatorial = numero * y;
+    }
+
+    return fatorial;
 }
